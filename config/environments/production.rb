3aeb5e3ed6f403a7c6dadf7be292e28139459014
David Walsh <davidliamwalsh@yahoo.co.uk>
2020-07-24 16:15:25 +0100

    added production heroku endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: "https://my-blogger.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://ruby-my-blogger.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.postmarkapp.com",
